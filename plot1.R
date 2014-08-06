516397388b9e90dc7e4347ad965672698a99a0b9
garrens <scott@garrenshay.com>
2014-08-06 07:16:53 -0400

    yikes, another bug!

--- plot1.R
+++ plot1.R
@@ -1,12 +1,13 @@
 ## set working directory
 setwd("~/Exp Data Analysis/project 1")
-##read data file
-power<-read.csv("household_power_consumption.txt", sep=";", na.strings = "?", as.is=TRUE)
-##select assigned dates
-power2<-power[power$Date=="2/2/2007"|power$Date=="1/2/2007",]
-##convert Date and Time to Date and Time data types from text
-power2$Date<-as.Date(power2$Date,format="%d/%m/%Y")
-power2$Time<-strptime(power2$Time,"%H:%M:%S")
+##if data does not exist read it in and convert date and times to proper data types
+if( !exists("power_data")) {
+  print("reading data")
+  power_data<-read.csv("household_power_consumption.txt", sep=";", na.strings = "?", as.is=TRUE)
+  power2<-power_data[power_data$Date=="2/2/2007"|power_data$Date=="1/2/2007",]
+  power2$Date<-as.Date(power2$Date,format="%d/%m/%Y")
+  power2$DateTime<-strptime(paste(power2$Date, power2$Time),format = "%Y-%m-%d %H:%M:%S")
+}
 ##select PNG as plot output type
 png(filename = "plot1.png", width = 480, height = 480, units = "px")
 ##draw histogram
